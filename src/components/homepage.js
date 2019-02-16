d0309b5100b530ea7f5e06329a94362e98864760
Dogancan Kasap <dgncn.ksp@gmail.com>
2019-02-17 00:06:25 +0300

    css layout 2

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -8,17 +8,39 @@ class Homepage extends Component {
           <h1>Youtube Keyword Feed</h1>
         </div>
         <div className="main-content">
+          <div className="search-container">
+            <div className="search-fields">
+              <input type="text" name="keyword" />
+              <button>Ekle</button>
+            </div>
+          </div>
           <div className="container">
             <div className="container-item">
-              <img src="#" />
+              <div className="item-detail">
+                <img src="#" alt="k" />
+                <div className="title">
+                  <a href="http://youtube.com">Bu başlık</a>
+                </div>
+              </div>
+              <div className="item-detail">
+                <img src="#" alt="k" />
+                <div className="title">
+                  <a href="http://youtube.com">Bu başlık</a>
+                </div>
+              </div>
+              <div className="item-detail">
+                <img src="#" alt="k" />
+                <div className="title">
+                  <a href="http://youtube.com">Bu başlık</a>
+                </div>
+              </div>
+            </div>
+            <div className="container-item">
+              <img src="#" alt="k" />
               <div className="title">Bu başlık</div>
             </div>
             <div className="container-item">
-              <img src="#" />
-              <div className="title">Bu başlık</div>
-            </div>
-            <div className="container-item">
-              <img src="#" />
+              <img src="#" alt="k" />
               <div className="title">Bu başlık</div>
             </div>
           </div>
