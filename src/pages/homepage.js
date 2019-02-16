9ba7739eb200d7212e1bf04b90a81426ee6f4ee7
Dogancan Kasap <dgncn.ksp@gmail.com>
2019-02-16 19:55:35 +0300

    initial

---
+++ src/pages/homepage.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "../App.css";
+
+class Homepage extends Component {
+  render() {
+    return (
+      <div className="">
+        <p>asd</p>
+        <p>asdqw</p>
+      </div>
+    );
+  }
+}
+
+export default Homepage;
